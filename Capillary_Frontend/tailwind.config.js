f8a5c7c14fc53135708675aad4ddefebe30d0ae6
Aswinrajr <aswinrajr07@gmail.com>
2025-01-02 13:34:49 +0530

    scollbar removal code added

--- Capillary_Frontend/tailwind.config.js
+++ Capillary_Frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -11,5 +13,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwind-scrollbar")],
+  plugins: [tailwindScrollbar],
 };
